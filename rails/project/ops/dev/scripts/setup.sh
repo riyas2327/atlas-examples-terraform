8db6362842ccaaa60c7637c18d7b1f9e45bbc654
Elijah Caine <elijahcainevm@gmail.com>
2015-03-24 14:16:49 -0700

    Dev envrionment working with auto deployment

--- rails/project/ops/dev/scripts/setup.sh
+++ rails/project/ops/dev/scripts/setup.sh
@@ -1,17 +1,17 @@
 sudo apt-get -y update
 # Dev Stuff
-sudo apt-get -y install ruby1.9.3 
-sudo gem install rails
+sudo apt-get -y install ruby1.9.3 build-essential libsqlite3-dev
+sudo gem install --no-ri --no-rdoc rails
 cd /vagrant/project/app; bundle install
 
 # Ops Stuff
-sudo apt-ke adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
 sudo cp /vagrant/project/ops/prod/passenger.list /etc/apt/sources.list.d/passenger.list
 sudo chown root:root /etc/apt/sources.list.d/passenger.list
 sudo chmod 600 /etc/apt/soruces.list.d/passenger.list
-sudo apt-get update
-sudo apt-get install libapache2-mod-passenger
-sudo cp /vagrant/project/ops/prod/apache.conf /etc/apache2/apache2.conf
+sudo apt-get -y update
+sudo apt-get -y install apache2 libapache2-mod-passenger
+sudo cp /vagrant/project/ops/prod/apache2.conf /etc/apache2/apache2.conf
 sudo a2enmod passenger
 sudo apache2ctl restart
 
