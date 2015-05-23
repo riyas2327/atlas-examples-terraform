163bdc823c8a2b7bcf2fa7c259b1871a0cf381f1
Jon Benson <bensojon@gmail.com>
2015-05-22 21:13:57 -0700

    Update example configs

--- getting-started/ops/example-infrastructure.tf
+++ getting-started/ops/example-infrastructure.tf
@@ -8,6 +8,35 @@ provider "aws" {
     region = "us-east-1"
 }
 
+resource "aws_security_group" "allow_all" {
+    name = "allow_all"
+    description = "Allow all inbound traffic"
+
+    tags {
+        Name = "allow_all"
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "web" {
+    instance_type = "t1.micro"
+    ami = "ami-408c7f28"
+    security_groups = ["${aws_security_group.allow_all.name}"]
+
+    tags {
+        Name = "web_${count.index+1}"
+    }
+
+    # This will create 2 instances
+    count = 2
+}
+
 resource "aws_elb" "web" {
     name = "terraform-example-elb"
 
@@ -21,15 +50,16 @@ resource "aws_elb" "web" {
         lb_protocol = "http"
     }
 
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 5
+        target = "TCP:80"
+        interval = 10
+    }
+
+    security_groups = ["${aws_security_group.allow_all.id}"]
+
     # The instances are registered automatically
     instances = ["${aws_instance.web.*.id}"]
-}
-
-
-resource "aws_instance" "web" {
-    instance_type = "t1.micro"
-    ami = "ami-408c7f28"
-
-    # This will create 2 instances
-    count = 2
 }
