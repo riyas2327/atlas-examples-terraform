18cb4f67860c43382fdcdbe01ff6e34331bb20a7
Jon Benson <bensojon@gmail.com>
2015-05-25 21:57:23 -0700

    Update 4 spaces to 2 spaces

--- getting-started/ops/example-infrastructure.tf
+++ getting-started/ops/example-infrastructure.tf
@@ -1,65 +1,65 @@
 provider "atlas" {
-    token = "ATLAS_TOKEN_HERE"
+  token = "ATLAS_TOKEN_HERE"
 }
 
 provider "aws" {
-    access_key = "ACCESS_KEY_HERE"
-    secret_key = "SECRET_KEY_HERE"
-    region = "us-east-1"
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region = "us-east-1"
 }
 
 resource "aws_security_group" "allow_all" {
-    name = "allow_all"
-    description = "Allow all inbound traffic"
+  name = "allow_all"
+  description = "Allow all inbound traffic"
 
-    tags {
-        Name = "allow_all"
-    }
+  tags {
+    Name = "allow_all"
+  }
 
-    ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web" {
-    instance_type = "t1.micro"
-    ami = "ami-408c7f28"
-    security_groups = ["${aws_security_group.allow_all.name}"]
+  instance_type = "t1.micro"
+  ami = "ami-408c7f28"
+  security_groups = ["${aws_security_group.allow_all.name}"]
 
-    tags {
-        Name = "web_${count.index+1}"
-    }
+  tags {
+    Name = "web_${count.index+1}"
+  }
 
-    # This will create 2 instances
-    count = 2
+  # This will create 2 instances
+  count = 2
 }
 
 resource "aws_elb" "web" {
-    name = "terraform-example-elb"
+  name = "terraform-example-elb"
 
-    # The same availability zone as our instances
-    availability_zones = ["${aws_instance.web.*.availability_zone}"]
+  # The same availability zone as our instances
+  availability_zones = ["${aws_instance.web.*.availability_zone}"]
 
-    listener {
-        instance_port = 80
-        instance_protocol = "http"
-        lb_port = 80
-        lb_protocol = "http"
-    }
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
 
-    health_check {
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-        timeout = 5
-        target = "TCP:80"
-        interval = 10
-    }
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    target = "TCP:80"
+    interval = 10
+  }
 
-    security_groups = ["${aws_security_group.allow_all.id}"]
+  security_groups = ["${aws_security_group.allow_all.id}"]
 
-    # The instances are registered automatically
-    instances = ["${aws_instance.web.*.id}"]
+  # The instances are registered automatically
+  instances = ["${aws_instance.web.*.id}"]
 }
