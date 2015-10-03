d6ad8ec5249bde900dbf1ab129043db25eb4ec77
Cameron Stokes <cameron@cameronstokes.com>
2015-10-03 09:37:45 -0700

    nomad-cluster: Fixed spaces and removed unused vars

--- nomad-cluster/aws-beginner-nomad-cluster/terraform/variables.tf
+++ nomad-cluster/aws-beginner-nomad-cluster/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {
-	default = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "atlas_token" { }
@@ -12,29 +12,25 @@ variable "atlas_environment" {}
 // Instance Variables
 //
 variable "source_ami" {
-	default = "ami-9a562df2"
+  default = "ami-9a562df2"
 }
 variable "key_name" {}
 
 variable "instance_type" {
-	default = "t2.medium"
+  default = "t2.medium"
 }
 
 variable "nomad_bootstrap_expect" {
-	default = "3"
+  default = "3"
 }
 
 //
 // VPC Variables
 //
 variable "vpc_cidr" {
-	default = "172.31.0.0/16"
+  default = "172.31.0.0/16"
 }
 
 variable "vpc_cidrs" {
-	default = "172.31.0.0/20,172.31.16.0/20,172.31.32.0/20"
-}
-
-variable "subnets" {
-	default = "us-east-1a,us-east-1b,us-east-1d"
+  default = "172.31.0.0/20,172.31.16.0/20,172.31.32.0/20"
 }
