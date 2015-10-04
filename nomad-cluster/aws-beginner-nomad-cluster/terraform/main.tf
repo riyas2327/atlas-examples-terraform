1976c2ac5ef1adaf24aefd2a98a795ba566b2a79
Cameron Stokes <cameron@cameronstokes.com>
2015-10-04 11:27:08 -0700

    ~ nomad-cluster: moved outputs to main.tf

--- nomad-cluster/aws-beginner-nomad-cluster/terraform/main.tf
+++ nomad-cluster/aws-beginner-nomad-cluster/terraform/main.tf
@@ -8,3 +8,19 @@ module "shared" {
   source = "../../shared"
   key_name = "${var.key_name}"
 }
+
+output "nomad_0" {
+  value = "${aws_instance.nomad_0.private_ip} - ${aws_instance.nomad_0.public_ip}"
+}
+
+output "nomad_1" {
+  value = "${aws_instance.nomad_1.private_ip} - ${aws_instance.nomad_1.public_ip}"
+}
+
+output "nomad_2" {
+  value = "${aws_instance.nomad_2.private_ip} - ${aws_instance.nomad_2.public_ip}"
+}
+
+output "nomad clients" {
+  value = "${join(", ", aws_instance.nomad_client.*.public_ip)}"
+}
