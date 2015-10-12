38c16e40a1199010638abe88d7f36b012c7fd1d7
Jay Wallace <jaycwallace@gmail.com>
2015-10-11 23:28:22 -0700

    first revisions for pipeline

--- HAProxy-Nodejs/app/server.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Atlas Example Complete!");
-  response.end();
-}).listen(8888);
