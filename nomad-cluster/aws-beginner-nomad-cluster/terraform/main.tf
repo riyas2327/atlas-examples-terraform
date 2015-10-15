aeaeea90cfcc19ebbbba75fc678a0071c81256cd
Cameron Stokes <cameron@cameronstokes.com>
2015-10-15 12:53:34 -0400

    ~ nomad-cluster: common inputs and structure

--- nomad-cluster/aws-beginner-nomad-cluster/terraform/main.tf
+++ nomad-cluster/aws-beginner-nomad-cluster/terraform/main.tf
@@ -1,14 +1,33 @@
+//
+// Providers & Modules
+//
 provider "aws" {
-  region     = "${var.region}"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  region = "${var.region}"
 }
 
 module "shared" {
-  source = "../../shared"
+  source   = "../../shared"
   key_name = "${var.key_name}"
 }
 
+//
+// Variables
+//
+variable "atlas_environment" { default = "nomad-cluster" }
+
+variable "region"        { default = "us-east-1" }
+variable "source_ami"    { default = "ami-9a562df2" }
+variable "key_name"      { default = "atlas-example" }
+variable "instance_type" { default = "t2.micro" }
+
+variable "vpc_cidr"  { default = "172.31.0.0/16" }
+variable "vpc_cidrs" { default = "172.31.0.0/20,172.31.16.0/20,172.31.32.0/20" }
+
+variable "nomad_bootstrap_expect" { default = "3" }
+
+//
+// Outputs
+//
 output "nomad_0" {
   value = "${aws_instance.nomad_0.private_ip} - ${aws_instance.nomad_0.public_ip}"
 }
