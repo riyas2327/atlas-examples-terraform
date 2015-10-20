7e88d4575b0e2e3f0f420deb65e11d2d32bec883
Jon Benson <bensojon@gmail.com>
2015-10-20 14:19:50 -0700

    Fix ports on haproxy

--- infrastructures/terraform/aws/network/haproxy/main.tf
+++ infrastructures/terraform/aws/network/haproxy/main.tf
@@ -19,17 +19,31 @@ resource "aws_security_group" "haproxy" {
   tags { Name = "${var.name}" }
 
   ingress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["${var.vpc_cidr}"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 8080
+    to_port     = 8080
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 1936
+    to_port     = 1936
+    cidr_blocks = ["${var.vpc_cidr}"]
   }
 
   egress {
