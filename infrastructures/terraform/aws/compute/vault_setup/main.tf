e4ef10d2a48e1fd11a0b6f92320251ebe5a872c4
Jon Benson <bensojon@gmail.com>
2015-10-21 00:31:19 -0700

    Remove env var

--- infrastructures/terraform/aws/compute/vault_setup/main.tf
+++ infrastructures/terraform/aws/compute/vault_setup/main.tf
@@ -71,11 +71,6 @@ TOKEN=$(vault token-create -policy=${var.name} | grep ^token[^_] | awk '{print $
 echo "Saving Vault token to Consul"
 curl -XPUT http://127.0.0.1:8500/v1/kv/service/${var.name}/vault-token -d $TOKEN
 
-echo "Saving Vault token in env var for Consul Template..."
-BASHRCPATH=~/.bashrc
-sh -c echo -e "\n" >> $BASHRCPATH
-echo "export VAULT_TOKEN=$TOKEN" | tee -a $BASHRCPATH
-
 if [ -e ~/.vault-token ]; then
   echo "Shredding vault-token"
   shred -u -z ~/.vault-token
