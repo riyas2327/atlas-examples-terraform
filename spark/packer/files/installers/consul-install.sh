8eb7a34e9743f76c22b955418ec8dc04d81e1bee
Cameron Stokes <cameron@cameronstokes.com>
2015-10-23 11:13:58 -0700

    ~ spark: rework apt-get update/upgrade to fix java install errors

--- spark/packer/files/installers/consul-install.sh
+++ spark/packer/files/installers/consul-install.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-sudo apt-get -y update
-
 # install consul
-echo Installing dependencies...
+echo "Installing dependencies..."
 sudo apt-get install -y unzip
 sudo apt-get install -y curl
+
 echo "Fetching consul..."
 cd /tmp/
 wget -q https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip -O consul.zip
+
 echo "Installing consul..."
 unzip consul.zip
 sudo chmod +x consul
