4a4ca3f24f1112bfa6c972097aa1d5af3a759cce
Cameron Stokes <cameron@cameronstokes.com>
2015-10-25 11:23:43 -0700

    ~ consul-cluster: ensure permissions for parent consul path

--- consul-cluster/shared/consul/installers/consul_install.sh
+++ consul-cluster/shared/consul/installers/consul_install.sh
@@ -18,17 +18,22 @@ wget -q https://dl.bintray.com/mitchellh/consul/${CONSUL_VERSION}_linux_amd64.zi
 
 echo "Installing consul..."
 unzip consul.zip
+rm consul.zip
 sudo chmod +x consul
 sudo mv consul /usr/bin/consul
 sudo mkdir -m 0600 /etc/consul.d
 
 # setup consul directories
-sudo mkdir -p /opt/consul/data
+sudo mkdir -m 0600 -p /opt/consul
+sudo mkdir /opt/consul/data
+sudo mkdir /opt/consul/web
 
 # install consul-web
 echo "Fetching consul-web..."
 wget -q https://dl.bintray.com/mitchellh/consul/${CONSUL_VERSION}_web_ui.zip -O consul-web.zip
 unzip consul-web.zip
-sudo mv dist /opt/consul/web
+rm consul-web.zip
+sudo mv dist/* /opt/consul/web
+rm -r dist
 
 echo "Consul installation complete."
