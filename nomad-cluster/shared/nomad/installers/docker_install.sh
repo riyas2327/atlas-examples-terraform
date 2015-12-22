76bdd3fa70d1260988bf03e8664d7f5470ec6608
Jay Wallace <jaycwallace@gmail.com>
2015-12-21 23:56:25 -0800

    Update docker_install.sh

--- nomad-cluster/shared/nomad/installers/docker_install.sh
+++ nomad-cluster/shared/nomad/installers/docker_install.sh
@@ -2,3 +2,4 @@
 
 sudo curl -sSL https://get.docker.com/ | sh
 sudo sh -c "echo \"DOCKER_OPTS='--dns 172.17.42.1 --dns 8.8.8.8 --dns-search service.consul'\" >> /etc/default/docker"
+sudo service docker restart
