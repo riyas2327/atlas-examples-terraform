11d818d4fb4fe73a275feb9e6005cdabdd927484
Jon Benson <bensojon@gmail.com>
2016-01-08 14:29:15 -0800

    Pass keys as variable

--- nomad-cluster/shared/main.tf
+++ nomad-cluster/shared/main.tf
@@ -1,13 +1,3 @@
-variable "key_name" {}
-
 output "path" {
   value = "${path.module}"
-}
-
-output "public_key_path" {
-  value = "${path.module}/ssh_keys/${var.key_name}.pub"
-}
-
-output "private_key_path" {
-  value = "${path.module}/ssh_keys/${var.key_name}.pem"
 }
