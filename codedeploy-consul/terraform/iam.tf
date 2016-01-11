edf867d22a150307d03659eacb11a22f578e9ff1
Cameron Stokes <cameron@cameronstokes.com>
2016-01-11 10:51:59 -0800

    Rename IAM files to match codedeploy docs.

--- codedeploy-consul/terraform/iam.tf
+++ codedeploy-consul/terraform/iam.tf
@@ -1,9 +1,28 @@
 //
-// Instance Profile for CodeDeploy
+// Service role for CodeDeploy service
 //
-resource "aws_iam_instance_profile" "codedeploy" {
-  name  = "CodeDeployDemo-EC2-Instance-Profile"
-  roles = ["${aws_iam_role.codedeploy_instance.name}"]
+resource "aws_iam_role" "codedeploy_service_role" {
+  name = "CodeDeployServiceRole"
+  assume_role_policy = "${file("${module.shared.path}/codedeploy-agent/iam/CodeDeployDemo-Trust.json")}"
+}
+
+resource "aws_iam_policy_attachment" "codedeploy_service_role" {
+  name       = "CodeDeployServiceRole"
+  roles      = ["${aws_iam_role.codedeploy_service_role.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+//
+// Instance profiles for instances running the CodeDeploy agent
+//
+resource "aws_iam_role" "codedeploy_instance" {
+  name               = "CodeDeployDemo-EC2"
+  assume_role_policy = "${file("${module.shared.path}/codedeploy-agent/iam/CodeDeployDemo-EC2-Trust.json")}"
+}
+
+resource "aws_iam_policy" "codedeploy_instance" {
+  name   = "CodeDeployDemo-EC2-Permissions"
+  policy = "${file("${module.shared.path}/codedeploy-agent/iam/CodeDeployDemo-EC2-Permissions.json")}"
 }
 
 resource "aws_iam_policy_attachment" "codedeploy_instance" {
@@ -12,12 +31,7 @@ resource "aws_iam_policy_attachment" "codedeploy_instance" {
   policy_arn = "${aws_iam_policy.codedeploy_instance.arn}"
 }
 
-resource "aws_iam_policy" "codedeploy_instance" {
-  name   = "CodeDeployDemo-EC2-Permissions"
-  policy = "${file("${module.shared.path}/codedeploy-agent/iam/instance-profile-permissions.json")}"
-}
-
-resource "aws_iam_role" "codedeploy_instance" {
-  name               = "CodeDeployDemo-EC2"
-  assume_role_policy = "${file("${module.shared.path}/codedeploy-agent/iam/instance-profile-trust.json")}"
+resource "aws_iam_instance_profile" "codedeploy" {
+  name  = "CodeDeployDemo-EC2-Instance-Profile"
+  roles = ["${aws_iam_role.codedeploy_instance.name}"]
 }
