948035422bbe3cdbe5ab691e8abf35292720debc
Cameron Stokes <cameron@cameronstokes.com>
2016-01-21 19:37:19 -0800

    Tag VPC.

--- codedeploy-consul/terraform/networks.tf
+++ codedeploy-consul/terraform/networks.tf
@@ -1,6 +1,10 @@
 resource "aws_vpc" "main" {
   cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = true
+
+  tags {
+    Name = "codedeploy_example"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
