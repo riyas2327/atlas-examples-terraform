45458c984027e45aabbaf5503ecd8f8aea86d32b
Cameron Stokes <cameron@cameronstokes.com>
2016-08-22 19:03:38 -0700

    Upgrade Consul to 0.6.4 and use built-in ui.

--- nomad-cluster/shared/consul/installers/consul_install.sh
+++ nomad-cluster/shared/consul/installers/consul_install.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-CONSUL_VERSION=0.6.0
+CONSUL_VERSION=0.6.4
 
 sudo apt-get -y update
 
@@ -26,11 +26,5 @@ sudo mkdir -pm 0600 /etc/consul.d
 # setup consul directories
 sudo mkdir -pm 0600 /opt/consul
 sudo mkdir -p /opt/consul/data
-sudo mkdir -p /opt/consul/web
-
-# install consul-web
-echo "Fetching consul-web..."
-wget -q https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_web_ui.zip -O consul-web.zip
-sudo unzip consul-web.zip -d /opt/consul/web
 
 echo "Consul installation complete."
