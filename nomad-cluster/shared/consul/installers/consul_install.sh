eaef2df90b3c340b7aea54b87964c35f88331bea
Cameron Stokes <cameron@cameronstokes.com>
2016-10-31 09:51:14 -0700

    Install consul-template

--- nomad-cluster/shared/consul/installers/consul_install.sh
+++ nomad-cluster/shared/consul/installers/consul_install.sh
@@ -3,11 +3,15 @@
 set -ex
 
 CONSUL_VERSION=0.7.0
+CONSUL_TEMPLATE_VERSION=0.16.0
 
-sudo apt-get -qq -y update
+#######################################
+# CONSUL
+#######################################
 
 # install dependencies
-echo "Installing dependencies..."
+echo "Installing consul dependencies..."
+sudo apt-get -qq -y update
 sudo apt-get install -qq -y unzip wget
 
 # install consul
@@ -28,3 +32,26 @@ sudo mkdir -pm 0600 /opt/consul
 sudo mkdir -p /opt/consul/data
 
 echo "Consul installation complete."
+
+#######################################
+# CONSUL-TEMPLATE
+#######################################
+
+# install dependencies
+echo "Installing consul-template dependencies..."
+sudo apt-get -qq -y update
+sudo apt-get install -qq -y unzip wget
+
+# install consul-template
+echo "Fetching consul-template..."
+cd /tmp/
+
+wget -q https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip -O consul-template.zip
+
+echo "Installing consul-template..."
+unzip consul-template.zip
+rm consul-template.zip
+sudo chmod +x consul-template
+sudo mv consul-template /usr/bin/consul-template
+
+echo "Consul-template installation complete."
